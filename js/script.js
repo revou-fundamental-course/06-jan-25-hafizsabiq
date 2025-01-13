1361dc7d2d69f781b429c9f4a182c309670d0fbf
hafizsabiq <hafizsabiqulhaq@gmail.com>
2025-01-13 22:14:24 +0700

    Add preview message

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-window.onload = function() {
+// Fungsi Input nama
+function nameInput() {
     var userName = prompt("Please enter your name:");
     var nameSpan = document.getElementById("name");
     if (userName) {
         if (nameSpan) {
-            nameSpan.textContent = userName;
+            nameSpan.innerHTML = userName;
         }
     } else {
         if (nameSpan) {
-            nameSpan.textContent = "Visitor";
+            nameSpan.innerHTML = "Visitor";
         }
     }
-};
+}
+
+// Fungsi Input nama terbuka saat website dimuat
+window.onload = nameInput;
+
+// Tombol ganti nama
+document.getElementById("Change Name").addEventListener("click", function () {
+    nameInput();
+});
