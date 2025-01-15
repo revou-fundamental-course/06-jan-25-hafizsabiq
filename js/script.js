3e98e51fd70dd745810ab01a5c6b53ceacad59b2
hafizsabiq <hafizsabiqulhaq@gmail.com>
2025-01-15 11:51:25 +0700

    Refine the navigation interaction

--- js/script.js
+++ js/script.js
@@ -21,6 +21,29 @@ document.getElementById("Change Name").addEventListener("click", function () {
     nameInput();
 });
 
+
+// Animasi scroll navigation
+document.querySelectorAll('nav a').forEach(anchor => {  
+    anchor.addEventListener('click', function(e) {  
+        e.preventDefault();  
+  
+        const targetId = this.getAttribute('href');  
+        const targetElement = document.querySelector(targetId);  
+        
+        // Y Offsett 100px agar tidak tertutup header
+        const offset = 100;  
+        const elementPosition = targetElement.getBoundingClientRect().top;  
+        const offsetPosition = elementPosition + window.pageYOffset - offset;  
+        
+        // Smooth scroll 
+        window.scrollTo({  
+            top: offsetPosition,  
+            behavior: 'smooth'  
+        });  
+    });  
+});  
+
+
 //Banner Auto Slide
 var slideIndex = 1;  
 showDivs(slideIndex);  
